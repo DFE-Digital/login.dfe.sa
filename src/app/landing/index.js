27771657facecb33735ddea865b888deb1f7f3e3
Rhys Bridges <bridgesr@uni.coventry.ac.uk>
2019-05-01 15:16:20 +0100

    NSA-2126- Migration switch off content

--- src/app/landing/index.js
+++ src/app/landing/index.js
@@ -5,14 +5,14 @@ const { asyncWrapper } = require('login.dfe.express-error-handling');
 
 const catchAll = require('./catchAll');
 const existingDSI = require('./existingDSi');
-const existingSA = require('./existingSA');
-const newUser = require('./newUser');
+const createAccount = require('./createAccount');
+
 const router = express.Router();
 
 const buildArea = () => {
   router.get('/existing-user', asyncWrapper(existingDSI.get));
-  router.get('/existing-secure-access-user', asyncWrapper(existingSA.get));
-  router.get('/new-user', asyncWrapper(newUser.get));
+  router.get('/create-account', asyncWrapper(createAccount.get));
+
   router.get('/', asyncWrapper(catchAll.get));
 
   router.get('/*', asyncWrapper((req, res) => {
